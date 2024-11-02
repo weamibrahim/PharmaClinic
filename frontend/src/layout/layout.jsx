5f5881f21cde19eb27cb7310e2621244330d20c4
weam <weamibrahim46@gmail.com>
2024-11-02 23:11:29 +0200

    remove , from layout

--- frontend/src/layout/layout.jsx
+++ frontend/src/layout/layout.jsx
@@ -1,4 +1,4 @@
-import React,  from 'react';
+import React  from 'react';
 import Sidebar from '../Components/sidebar';
 import { Outlet } from 'react-router-dom';
 import ModelForChat from '../Components/ModelForChat';
